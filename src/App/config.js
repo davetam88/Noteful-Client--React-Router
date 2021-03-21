8f39908ad3c7b875d9e091dbb0a54b7bfc87ff9d
David Tam <davetam88@gmail.com>
2021-03-21 15:10:35 -0400

    fixed warning mesasge

--- src/App/config.js
+++ src/App/config.js
@@ -1,6 +1,7 @@
-export default {
+const config = {
   API_ENDPOINT: `http://localhost:8000`,
   // API_ENDPOINT: `http://localhost:9090`,
   API_KEY: '$2a$10$ra1z0n2XnSnbMP/ipTMHeOqqrI7i8Rssm/z8MHTxgb7LamV7LpfXu',
-}
+};
 
+export default config;
