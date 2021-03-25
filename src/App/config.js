eef169780c94737ec3e044282b9a5fccbb5ba372
David Tam <davetam88@gmail.com>
2021-03-25 17:13:35 -0400

    vercel push version

--- src/App/config.js
+++ src/App/config.js
@@ -1,6 +1,6 @@
 const config = {
-  API_ENDPOINT: `http://localhost:8000`,
-  // API_ENDPOINT: `http://localhost:9090`,
+  // API_ENDPOINT: `http://localhost:8000`,
+  API_ENDPOINT: `https://enigmatic-tundra-38299.herokuapp.com/`,
   API_KEY: '$2a$10$ra1z0n2XnSnbMP/ipTMHeOqqrI7i8Rssm/z8MHTxgb7LamV7LpfXu',
 };
 
